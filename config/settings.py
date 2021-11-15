ff5d8bf1892e0133a27d08db792eb12796721414
alfranc <alexfranco12@gmail.com>
2021-11-14 19:40:32 -0600

    fixed typo

--- config/settings.py
+++ config/settings.py
@@ -164,7 +164,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT=os.path.join(BASE_DIR, "staticfiles/")
+STATIC_ROOT=os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
