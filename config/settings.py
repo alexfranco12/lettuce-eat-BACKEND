033e5d84611334b357a9a26b7d49912970089e91
alfranc <alexfranco12@gmail.com>
2021-11-14 20:05:45 -0600

    refactored settings.py

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,6 @@ import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -117,7 +116,6 @@ DATABASES = {
   }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -136,18 +134,13 @@ AUTH_PASSWORD_VALIDATORS = [
   },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 # Default primary key field type
